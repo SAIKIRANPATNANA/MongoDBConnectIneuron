517365fabb37de64c4f616df479315d31a289e1c
SAIKIRANPATANA <saikiranpatnana5143@gmail.com>
2024-01-06 17:19:23 +0530

    updated

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 __version__ = "0.0.1"
 REPO_NAME = "MongoDBConnectIneuron"
-PKG_NAME= "mayyaConnect"
+PKG_NAME= "PSKConnect"
 AUTHOR_USER_NAME = "SAIKIRANPATNANA"
 AUTHOR_EMAIL = "saikiranpatnana5143@gmail.com"
 
